fcf8280a3487bd82c47332dcd4209e1b72a7d145
Val Currie <contact@valcuri.email>
2021-05-03 14:56:10 -0500

    initial file layout is set -- so is some overall site structure

--- README.md
+++ README.md
@@ -2,3 +2,13 @@
 ### as implemented by: Devin Garcia, Jonathan Watson, Val Currie
 
 [click here for ambience](https://www.youtube.com/watch?v=rv3Nl-Od9YU)
+
+## Data Components
+- player
+- team
+- score
+
+## Screens
+- intro
+- setup
+- game
---
+++ scripts/truncheons.js
@@ -0,0 +1,19 @@
+export const truncheonsHTML = () => {
+  // TODO dont always render all three screens
+  return /*html*/ `
+  <article> ${introHTML()} </article>
+  <article> ${setupHTML()} </article>
+  <article> ${gameHTML()} </article>
+
+    `;
+};
+
+export const introHTML = () => {
+  return /*html*/ ``;
+};
+export const setupHTML = () => {
+  return /*html*/ ``;
+};
+export const gameHTML = () => {
+  return /*html*/ ``;
+};
