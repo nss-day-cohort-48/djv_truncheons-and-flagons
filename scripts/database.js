a57323884895caf2a0a832bc1ecbae99330b0173
Val Currie <contact@valcuri.email>
2021-05-03 15:37:18 -0500

    added more dummy data

--- scripts/database.js
+++ scripts/database.js
@@ -1,3 +1,8 @@
+// base time value for mock API entries
+const rightNow = new Date();
+// base time + 100 seconds (100000ms)
+const hundredSecondsLater = new Date(rightNow.getTime() + 100000);
+
 const database = {
   playerBuilder: {
     firstName: null,
@@ -20,11 +25,67 @@ const database = {
       lastName: "Doe",
       teamId: 1,
     },
+    {
+      id: 2,
+      firstName: "Dohn",
+      lastName: "Joe",
+      teamId: 1,
+    },
+    {
+      id: 3,
+      firstName: "Marty",
+      lastName: "McFly",
+      teamId: 1,
+    },
+    {
+      id: 4,
+      firstName: "French",
+      lastName: "Montana",
+      teamId: 2,
+    },
+    {
+      id: 5,
+      firstName: "Rick",
+      lastName: "Sanchez",
+      teamId: 2,
+    },
+    {
+      id: 6,
+      firstName: "Charles",
+      lastName: "Darlose",
+      teamId: 2,
+    },
+    {
+      id: 7,
+      firstName: "Fake",
+      lastName: "Name",
+      teamId: 3,
+    },
+    {
+      id: 8,
+      firstName: "Cornell",
+      lastName: "West",
+      teamId: 3,
+    },
+    {
+      id: 9,
+      firstName: "Bond",
+      lastName: "Jamesbond",
+      teamId: 3,
+    },
   ],
   teams: [
     {
       id: 1,
       name: "The Red Barons",
+    },
+    {
+      id: 2,
+      name: "The Flo Rida Fan Club",
+    },
+    {
+      id: 3,
+      name: "REDACTED",
     },
   ],
   scores: [
@@ -32,7 +93,41 @@ const database = {
       id: 1,
       teamId: 1,
       score: 0,
-      timestamp: Date.now(),
+      timestamp: rightNow,
+    },
+    {
+      id: 2,
+      teamId: 2,
+      score: 0,
+      timestamp: rightNow,
+    },
+    {
+      id: 3,
+      teamId: 3,
+      score: 0,
+      timestamp: rightNow,
+    },
+    {
+      id: 4,
+      teamId: 1,
+      score: 0,
+      timestamp: hundredSecondsLater,
+    },
+    {
+      id: 5,
+      teamId: 2,
+      score: 0,
+      timestamp: hundredSecondsLater,
+    },
+    {
+      id: 6,
+      teamId: 3,
+      score: 0,
+      timestamp: hundredSecondsLater,
     },
   ],
 };
+
+export const getPlayers = () => [...database.players];
+export const getScores = () => [...database.scores];
+export const getTeams = () => [...database.teams];
