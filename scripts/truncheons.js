34167c1003650e0e26654b20c9cd90c93590094b
Val Currie <contact@valcuri.email>
2021-05-04 09:23:16 -0500

    added very basic leaderboard

--- scripts/truncheons.js
+++ scripts/truncheons.js
@@ -9,7 +9,19 @@ export const truncheonsHTML = () => {
 };
 
 export const introHTML = () => {
-  return /*html*/ ``;
+  return /*html*/ `
+  <h1 class="logo">Truncheons & Flagons</h1>
+  <button class="newGameButton">Set Up New Game</button>
+  <div>
+    <h3>Current Leaderboard</h3>
+    <ul>
+      <li> #1: The Flo Rida Fan Club -- 4 points</li>
+      <li> #2: The Red Barons -- 2 points</li>
+      <li> #3: REDACTED -- 0 points</li>
+    </ul>
+  </div>
+
+  `;
 };
 export const setupHTML = () => {
   return /*html*/ ``;
