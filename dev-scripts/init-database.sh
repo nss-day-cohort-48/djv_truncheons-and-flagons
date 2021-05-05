3e8e348396faf3970b10d1195f34827bdae50288
Val Currie <contact@valcuri.email>
2021-05-05 14:08:02 -0500

    moved persistent data to db.json

---
+++ dev-scripts/init-database.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+confirm() {
+	echo "About to do: $1"
+	read -p "Are you sure? y or [n] => " -n 1 -r
+	echo
+	# REPLY is the default variable for read
+	if [[ $REPLY =~ ^[Yy]$ ]]; then
+		eval "$1"
+	fi
+}
+
+DBFILE="./api/db.json"
+INITIALDB="./dev-scripts/db.json.init"
+
+[[ -f $DBFILE ]] || (echo "Can't find $DBFILE" && exit 255)
+[[ -f $INITIALDB ]] || (echo "Can't find $INITIALDB" && exit 255)
+
+confirm "cat $INITIALDB > $DBFILE"
