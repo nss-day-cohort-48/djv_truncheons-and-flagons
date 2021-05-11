1cf8c3b0010e77695c56eef2df732256d6687abb
Val Currie <contact@valcuri.email>
2021-05-11 15:14:25 -0500

    fixed up intro screen

--- src/scripts/Truncheons.js
+++ src/scripts/Truncheons.js
@@ -1,23 +1,22 @@
-import { setupHTML } from "./ScreenSetup.js";
-// import { introHTML } from "./ScreenIntro.js";
-// import { gameHTML } from "./ScreenGame.js";
-// import { getGameState } from "./gameState.js";
+import {setupHTML} from "./ScreenSetup.js";
+import {introHTML} from "./ScreenIntro.js";
+// import {gameHTML} from "./ScreenGame.js";
+import {getGameState} from "./gameState.js";
 
 export const truncheonsHTML = () => {
-    // const gameState = getGameState();
+  const gameState = getGameState();
 
-    // 	if (gameState.intro) {
-    // 		return /*html*/ `
-    //       <article> ${introHTML()}</article>
-    //     `;
-    // 	} else if (!gameState.intro && gameState.setup && !gameState.playing) {
-    // 		return /*html*/ `
-    //         <article> ${setupHTML()}</article>
-    //       `;
-    // 	} else if (!gameState.intro && !gameState.setup && gameState.playing) {
-    // 		return /*html*/ `
-    //   <article> ${gameHTML()} </article>
-    //   `;
-    // 	}
-    return `<article> ${setupHTML()}</article>`;
+  if (gameState.intro) {
+    return /*html*/ `
+          <article> ${introHTML()}</article>
+        `;
+  } else if (!gameState.intro && gameState.setup && !gameState.playing) {
+    return /*html*/ `
+            <article> ${setupHTML()}</article>
+          `;
+    //   } else if (!gameState.intro && !gameState.setup && gameState.playing) {
+    //     return /*html*/ `
+    //           <article> ${gameHTML()} </article>
+    //           `;
+  }
 };
