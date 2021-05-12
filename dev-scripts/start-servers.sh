f072fddc09f8eb9707debce5e80091bd59de6484
Val Currie <contact@valcuri.email>
2021-05-12 12:34:51 -0500

    Now all you need to do is modify localserve!

--- dev-scripts/start-servers.sh
+++ dev-scripts/start-servers.sh
@@ -1,28 +1,29 @@
 #!/usr/bin/env bash
 
-# ============================================> VARS
-## CUSTOMIZE THESE FOR json-server
+# ========================================================================> VARS
 JSRV_LOGFILE="./dev-scripts/json-server.log"
-JSRV_PORT="8081"
-JSRV_DBFILE="./api/database.json"
-
-## CUSTOMIZE THESE FOR serve
 SRV_LOGFILE="./dev-scripts/serve.log"
-SRV_PORT="8080"
-SRV_SRCDIR="./src"
 
-# ============================================> FUNC
+## ------- FROM HERE ON OUT, IT'S ALL BASH: YOU'VE BEEN WARNED -------
+# ========================================================================> ARGS
+JSRV_PORT="$1"
+JSRV_DBFILE="$2"
+SRV_PORT="$3"
+SRV_SRCDIR="$4"
+
+# ========================================================================> FUNC
 pid_of() {
     RESULT=$(ps | grep -v 'grep' | grep "$1" | cut -d ' ' -f 2)
 }
 
-# ============================================> EXEC
+# ========================================================================> EXEC
 ## no errors aloud!!1!
 set -e
 
 # init stuff
 echo "Clearing the json-server log file: $JSRV_LOGFILE"     && echo "" > $JSRV_LOGFILE
 echo "Clearing the serve log file: $SRV_LOGFILE"            && echo "" > $SRV_LOGFILE
+echo
 
 # clear out stuck processes that might interfere with serving
 SERVE8081=$(pid_of "serve.*-l 8081")
@@ -34,4 +35,6 @@ echo "Started 'serve' on port $SRV_PORT -- see $SRV_LOGFILE for more details"
 json-server -p $JSRV_PORT --watch $JSRV_DBFILE > $JSRV_LOGFILE 2>&1 & 
 echo "Started 'json-server' on port $JSRV_PORT -- see $JSRV_LOGFILE for more details"
 
-echo -e "\nPress ctrl+c to exit!" && wait
+# go time!!!
+echo
+echo "Press ctrl+c to exit!" && wait
