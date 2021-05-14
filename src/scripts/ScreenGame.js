1743e7a8069d6eba63ce96b79599a96524a3bef9
Jonathan Watson <thejmdw@gmail.com>
2021-05-14 18:44:59 -0400

    fixed the bug we encountered and adjusted the placement of the winning team

--- src/scripts/ScreenGame.js
+++ src/scripts/ScreenGame.js
@@ -1,27 +1,45 @@
 import {
-    ScoreFormHTML,
-    winningResultsHTML,
-    sumbitResultsButton,
+	ScoreFormHTML,
+	winningResultsHTML,
+	sumbitResultsButton
 } from "./ScoreForm.js";
 import { ScoreBoardHTML } from "./ScoreBoard.js";
+import { getGameState } from "./gameState.js";
 
 export const gameHTML = () => {
-    return /*html*/ `
+	const currentGameState = getGameState();
+
+	if (currentGameState.end === true) {
+		return /*html*/ `
+    <div class="game">
+      <h1 class="logo">Truncheons & Flagons</h1>
+
+      <div class="results">
+        <div class="winningResults">${winningResultsHTML()}</div>
+        <div class="resetGame">${sumbitResultsButton()}</div>
+      </div>
+    
+
+      <div class="score-board" >
+        ${ScoreBoardHTML()}
+      </div>
+    </div>
+    `;
+	} else {
+		return /*html*/ `
   <div class="game">
     <h1 class="logo">Truncheons & Flagons</h1>
 
     <div class="score-form">
       ${ScoreFormHTML()}
-  </div>
+    </div>
+    </div>
 
-	<div class="score-board" >
-		${ScoreBoardHTML()}
+	  <div class="score-board" >
+		  ${ScoreBoardHTML()}
     </div>
-    </div>
+  </div>
     
-    <div class="results">
-    <div class="winningResults">${winningResultsHTML()}</div>
-    <div class="resetGame">${sumbitResultsButton()}</div>
-    </div>
   `;
+	}
 };
