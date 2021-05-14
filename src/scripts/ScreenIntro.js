013403d465083fd7ce48f7174d27ac1563ca0c64
Jonathan Watson <thejmdw@gmail.com>
2021-05-14 12:56:01 -0400

    added some font styling

--- src/scripts/ScreenIntro.js
+++ src/scripts/ScreenIntro.js
@@ -1,17 +1,17 @@
-import {leaderboardHTML} from "./Leaderboard.js";
-import {setupGame} from "./gameState.js";
+import { leaderboardHTML } from "./Leaderboard.js";
+import { setupGame } from "./gameState.js";
 
 document.addEventListener("click", (event) => {
-  if (event.target.id === "newGameButton") {
-    setupGame();
-  }
+	if (event.target.id === "newGameButton") {
+		setupGame();
+	}
 });
 
 export const introHTML = () => {
-  return /*html*/ `
+	return /*html*/ `
     <h1 class="logo">Truncheons & Flagons</h1>
     <div class="leaderboardContainer">
-      <h3 class="leaderboardHeader">Current Leaderboard</h3>
+      <h2 class="leaderboardHeader">Current Leaderboard</h2>
       ${leaderboardHTML()}
     </div>
     <div class="newGame" id="newGameButtonContainer">
