d78737e62214b404ad55e0669d358f8587a91a6d
Val Currie <contact@valcuri.email>
2021-05-14 13:02:31 -0500

    Dont render any leaderboard if there are no scores or teams

--- src/scripts/ScreenIntro.js
+++ src/scripts/ScreenIntro.js
@@ -11,7 +11,6 @@ export const introHTML = () => {
   return /*html*/ `
     <h1 class="logo">Truncheons & Flagons</h1>
     <div class="leaderboardContainer">
-      <h3 class="leaderboardHeader">Current Leaderboard</h3>
       ${leaderboardHTML()}
     </div>
     <div class="newGame" id="newGameButtonContainer">
